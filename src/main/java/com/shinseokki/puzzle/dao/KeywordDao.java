c7c852df3052ef72a7743bf9e7d028af20e9bcad
wooyong <zelda0@naver.com>
2015-12-02 15:30:23 +0900

    Keyword 수정

--- src/main/java/com/shinseokki/puzzle/dao/KeywordDao.java
+++ src/main/java/com/shinseokki/puzzle/dao/KeywordDao.java
@@ -1,6 +1,5 @@
 package com.shinseokki.puzzle.dao;
 
-import java.util.HashMap;
 import java.util.List;
 
 import com.shinseokki.puzzle.dto.Keyword;
@@ -9,16 +8,12 @@ public interface KeywordDao {
 
 	
 		public int addkeyword(Keyword keyword);
-		public Keyword getKeyword(String K_NAME); //Keyword 타입에 파라미터 값은 Keywordname
+		public Keyword getKeyword(); 
 		public int updateKeyword(Keyword keyword);
-		public int deleteKeyword(Keyword keyword);
-		public List<Keyword> getKeywords(HashMap map);
-		
+		public int deleteKeyword(String K_NAME);
+		public List<Keyword> getKeywords(int start, int end);
 		int getKeywordCount();
-		/*public int updateKeyword(Keyword keyword);
-		public int deleteKeyword(Keyword keyword);*/
-		/*public List<Keyword> getKeywords(HashMap map);*/// 총 유저의 수
-		/*int getKeywordCount();*///유저의 수
+		
 	
 	}
 
